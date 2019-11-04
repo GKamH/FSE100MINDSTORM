91544b11889865efb244ebe3ac6d715aa76fc491
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-04 10:28:50 -0700

    Add files via upload

---
+++ BackAndForth.m
@@ -0,0 +1,19 @@
+count = 1;
+while count <= 10
+    while brick.TouchPressed(1) == 0
+    end
+    brick.MoveMotorAngleRel('A', 50, 180, 'Brake');
+    brick.WaitForMotor('A'); 
+    brick.MoveMotorAngleRel('B', 50, -180, 'Brake');
+    brick.WaitForMotor('B'); 
+    brick.StopAllMotors('Coast');
+        while brick.TouchPressed(1) == 1
+        end
+            brick.MoveMotorAngleRel('A', 50, -180, 'Brake');
+            brick.WaitForMotor('A'); 
+            brick.MoveMotorAngleRel('B', 50, 180, 'Brake');
+            brick.WaitForMotor('B'); 
+            brick.StopAllMotors('Coast');
+            display(count);
+            count = count + 1;
+end
