f31b212bd0b98abe786b177fd59897785e901bce
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-04 10:37:37 -0700

    Update AngleTest.m

--- AngleTest.m
+++ AngleTest.m
@@ -1,5 +1,5 @@
 brick.ResetMotorAngle('A'); 
-%brick.MoveMotorAngleRel('A', 50, 360, 'Brake'); 
+%brick.MoveMotorAngleRel('A', 50, 360, 'Brake'); //test1
 %brick.WaitForMotor('A'); 
  
 brick.MoveMotorAngleRel('A', 50, -180, 'Brake'); 
