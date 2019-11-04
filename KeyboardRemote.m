394237bf8bb7e759d250e0b2b4714f4463957094
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-04 11:42:05 -0700

    Add files via upload

--- KeyboardRemote.m
+++ KeyboardRemote.m
@@ -1,16 +1,14 @@
-  
 global key
 InitKeyboard();
-
 while 1
     pause(0.1);
     switch key
         case 'uparrow'
             disp('Up Arrow Pressed!');
-            forwardSquare;
+            forwardSquare(brick);
         case 'downarrow'
             disp('Down Arrow Pressed!');
-            backwardSquare;
+            backwardSquare(brick);
         case 'leftarrow'
             disp('Left Arrow Pressed!');
             turnLeftAmt(brick, 90);
@@ -18,13 +16,12 @@ while 1
             disp('Right Arrow Pressed!');
             turnRightAmt(brick, 90);
         case 0 % No key is being pressed
-            disp('No Key Pressed!');
         case 'o'
             disp('Open claw');
-            claw(brick, 150);
+            claw(brick, -120);
         case 'c'
             disp('Close claw');
-            claw(brick, -150);
+            claw(brick, 120);
         case 'q'
             break;
     end
