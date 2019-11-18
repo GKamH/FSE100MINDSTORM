d89ba0ce296c4c4037b4511a9c32f6a3c8e2f9f2
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-18 08:53:44 -0700

    Update MovementAlgorithm.m

--- MovementAlgorithm.m
+++ MovementAlgorithm.m
@@ -1,15 +1,31 @@
+clearvars;
 sonic = sonicSensor(brick, 4);
 delta = readDistancef(sonic);
-while 1
-    if Wall(delta)
-        pause(0.1);
-        brick.ResetMotorAngle('A'); 
-        brick.MoveMotorAngleRel('A', 80, 720, 'Brake'); 
-    elseif (delta < 4) 
-        
-    elseif (delta >= 20)
-        
-    else
-        
+color = colorSensor(brick, 1);
+kappa = readColor(color);
+tries = 1;
+while kappa == white || kappa == red   
+    if kappa == red
+        brick.MoveMotorAngleRel('BC', 40, 0);
     end
+    if Wall(delta)
+       pause(0.1);
+       if tries == 1
+           turnRightAmt(brick, 90);
+           tries = 2;
+       elseif tries == 2
+           turnLeftAmt(brick, 180);
+           tries = 3;
+       elseif tries == 3
+           turnLeftAmt(brick, 90);
+       end
+    else
+        forwardDistance();
+        tries = 0;
+    end
+end
+if kappa == green
+    brick.MoveMotorAngleRel('D',  40, 180);
+elseif kappa == yellow
+    brick.MoveMotorAngleRel('D',  40, -180);
 end
