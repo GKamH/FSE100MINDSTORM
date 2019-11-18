f10ed943ab3778d00237038a2469deee44e63fec
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-18 11:04:30 -0700

    Update MovementAlgorithm.m

--- MovementAlgorithm.m
+++ MovementAlgorithm.m
@@ -3,7 +3,14 @@ delta = brick.UltrasonicDist(4);
 brick.SetColorMode(1, 2);
 kappa = brick.ColorCode(1);
 tries = 1;
-while kappa ~= 4
+stillGoing = 1;
+while stillGoing == 1
+    turnRightAmt(brick, 90);
+    while Wall(delta)
+       pause(0.1);
+       turnLeftAmt(brick, 90); 
+    end
+    kappa = brick.ColorCode(1);
     if kappa == 5 %red stop sign
         brick.MoveMotorAngleRel('BC', 40, 0);
         pause(3);
@@ -11,27 +18,11 @@ while kappa ~= 4
     elseif kappa == 4 %yellow
         brick.MoveMotorAngleRel('D',  40, -180);
         kappa = brick.ColorCode(1);
+    elseif kappa == 6 % green
+        brick.MoveMotorAngleRel('D',  40, -180);
+        stillGoing = 0;
     elseif kappa == 3 %white
-        brick.MoveMotorAngleRel('D',  40, 180);
-        kappa = brick.ColorCode(1);
-    end
-    if Wall(delta)
-       pause(0.1);
-       if tries == 1
-           turnRightAmt(brick, 90);
-           tries = 2;
-           kappa = brick.ColorCode(1);
-       elseif tries == 2
-           turnLeftAmt(brick, 180);
-           tries = 3;
-           kappa = brick.ColorCode(1);
-       elseif tries == 3
-           turnLeftAmt(brick, 90);
-           kappa = brick.ColorCode(1);
-       end
-    else
-        forwardDistance();
-        tries = 0;
+        forwardSquare(brick);
         kappa = brick.ColorCode(1);
     end
 end
