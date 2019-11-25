0beae844e86968e6cb5250610a35848b515f1dce
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-25 09:30:57 -0700

    Update turnLeftAmt.m

--- turnLeftAmt.m
+++ turnLeftAmt.m
@@ -1,8 +1,6 @@
 function turnLeftAmt(brick, deg)
 brick.ResetMotorAngle('BC');
-brick.MoveMotorAngleRel('C', 20, 50);
-brick.WaitForMotor('C');
+controlClutch(brick, "turn");
 brick.MoveMotorAngleRel('B', 40, -deg * 12.4);
 brick.WaitForMotor('B');
-brick.MoveMotorAngleRel('C', 20, -50);
-brick.WaitForMotor('C');
+controlClutch(brick, "straight");
