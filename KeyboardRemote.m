84371f86b8b75caa784d3bf44d3c7f3b9656b9a7
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-25 09:49:55 -0700

    Update KeyboardRemote.m

--- KeyboardRemote.m
+++ KeyboardRemote.m
@@ -3,25 +3,40 @@ InitKeyboard();
 while 1
     pause(0.1);
     switch key
+        % Use brick.StopMotor(Port) to stop a motor.
         case 'uparrow'
             disp('Up Arrow Pressed!');
             forwardSquare(brick);
+            disp("Next?");
         case 'downarrow'
             disp('Down Arrow Pressed!');
             backwardSquare(brick);
+            disp("Next?");
         case 'leftarrow'
             disp('Left Arrow Pressed!');
             turnLeftAmt(brick, 90);
+            disp("Next?");
         case 'rightarrow'
             disp('Right Arrow Pressed!');
             turnRightAmt(brick, 90);
+            disp("Next?");
         case 0 % No key is being pressed
         case 'o'
             disp('Open claw');
-            claw(brick, -120);
+            claw(brick, -180);
+            disp("Next?");
         case 'c'
             disp('Close claw');
-            claw(brick, 120);
+            claw(brick, 180);
+            disp("Next?");
+        case 'x'
+            %White = 6
+            %Red = 5
+            %Yellow = 4
+            %Green = 3
+            disp('Read color');
+            ReadColor(brick);
+            disp("Next?");
         case 'q'
             break;
     end
