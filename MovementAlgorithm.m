fb4769f6e5d83caedb5e38278ab564b5649cad02
GKamH <50024522+GKamH@users.noreply.github.com>
2019-11-25 09:10:37 -0700

    Update MovementAlgorithm.m

--- MovementAlgorithm.m
+++ MovementAlgorithm.m
@@ -22,10 +22,10 @@ while stillGoing == 1
         brick.MoveMotorAngleRel('D',  40, -180);
         forwardSquare(brick);
         kappa = brick.ColorCode(1);
-    elseif kappa == 6 % green
+    elseif kappa == 3 % green
         brick.MoveMotorAngleRel('D',  40, -180);
         stillGoing = 0;
-    elseif kappa == 3 %white
+    elseif kappa == 6 %white
         forwardSquare(brick);
         kappa = brick.ColorCode(1);
     end
